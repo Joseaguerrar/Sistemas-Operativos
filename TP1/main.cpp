20b526158da3ca3eaae2c8c4885c0a70a37834f1
joseaguerrar <joseaguerrar2017@gmail.com>
2025-04-12 11:58:56 -0600

    Simulación con fcfs

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include "utils.hpp"
+#include "scheduler.hpp"
 
 int main() {
-    // Entrada de prueba (podrías luego pedirla con cin si querés)
     std::string input = "98, 183, 37, 122, 14, 124, 65, 67 | FCFS | Head:53 | ASC";
 
-    std::cout << "Entrada: " << input << "\n";
-
-    // Parseo con utils
     auto tokens = split(input, '|');
 
     if(tokens.size() < 2){
@@ -20,15 +17,23 @@ int main() {
     int head = tokens.size() > 2 ? parseHead(tokens[2]) : 0;
     std::string direction = tokens.size() > 3 ? parseDirection(tokens[3]) : "ASC";
 
-    // Mostrar resultados
     std::cout << "Algoritmo: " << algorithm << "\n";
     std::cout << "Head inicial: " << head << "\n";
     std::cout << "Dirección: " << direction << "\n";
     std::cout << "Requests: ";
-    for (auto r : requests) {
-        std::cout << r << " ";
-    }
+    for (auto r : requests) std::cout << r << " ";
     std::cout << "\n";
+
+    int total = 0;
+
+    if(algorithm == "FCFS") {
+        total = fcfs(requests, head);
+    } else {
+        std::cerr << "Algoritmo no implementado todavía\n";
+        return 1;
+    }
+
+    std::cout << "Total del recorrido: " << total << "\n";
 
     return 0;
 }
